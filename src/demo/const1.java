5222491a153e91e480eac6905d4fdf2b164f695a
bhaskar8 <bhaskar8@DESKTOP-JTGCRUB>
2020-12-06 09:05:15 +0530

    pushing

---
+++ src/demo/const1.java
@@ -0,0 +1,24 @@
+package demo;
+
+public class const1 { 		//class
+	
+
+		int a =10 ;           // VAR.
+		String s="0";
+		
+		const1()  // conST.  no return type
+		{
+		a = 10;
+		s = "Bhaskar";
+		System.out.println(this.s);
+		}
+		
+		public void test()
+		{
+			System.out.println(s);
+			System.out.println(this.a);
+			System.out.println("runing method test ");
+			
+		}
+		
+}
